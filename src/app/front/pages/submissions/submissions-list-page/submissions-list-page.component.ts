88ccc88b3ed89f4b4a0fbe6b789f2fdb7483aa29
Maxime Hutinet <maxime@hutinet.fr>
2025-02-27 16:07:16 +0100

    Added search to submission list page.

--- src/app/front/pages/submissions/submissions-list-page/submissions-list-page.component.ts
+++ src/app/front/pages/submissions/submissions-list-page/submissions-list-page.component.ts
@@ -4,12 +4,16 @@ import {TableModule} from "primeng/table";
 import {InsuranceSubmissionWithBills} from '../../../../services/billkeeper-ws/submission/model';
 import {SubmissionWsService} from '../../../../services/billkeeper-ws/submission/submission-ws.service';
 import {LayoutService} from '../../../../services/layout.service';
-import {BillStatus} from '../../../../services/billkeeper-ws/bill/model';
 import {SubmissionsTableComponent} from '../../../components/submissions/submissions-table/submissions-table.component';
 import {ToastMessageService} from '../../../../services/toast-message.service';
 import {BillWsService} from '../../../../services/billkeeper-ws/bill/bill-ws.service';
 import {ValidationService} from '../../../../services/validation.service';
 import {TopBarComponent} from "../../../components/layout/top-bar/top-bar.component";
+import {FloatLabel} from 'primeng/floatlabel';
+import {IconField} from 'primeng/iconfield';
+import {InputIcon} from 'primeng/inputicon';
+import {InputText} from 'primeng/inputtext';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-submissions-list-page',
@@ -17,16 +21,21 @@ import {TopBarComponent} from "../../../components/layout/top-bar/top-bar.compon
     MainLayoutComponent,
     TableModule,
     SubmissionsTableComponent,
-    TopBarComponent
+    TopBarComponent,
+    FloatLabel,
+    IconField,
+    InputIcon,
+    InputText,
+    FormsModule
   ],
   templateUrl: './submissions-list-page.component.html',
   styleUrl: './submissions-list-page.component.scss'
 })
 export class SubmissionsListPageComponent {
 
-  protected readonly BillStatus = BillStatus;
-
   submissions: InsuranceSubmissionWithBills[] = [];
+  filteredSubmissions: InsuranceSubmissionWithBills[] = [];
+  searchKeyword: string | undefined;
 
 
   constructor(
@@ -46,11 +55,20 @@ export class SubmissionsListPageComponent {
     try {
       await this.layoutService.withPageLoading(async () => {
         this.submissions = await this.submissionWsService.getAllSubmissions();
-        console.log(this.submissions)
+        this.searchKeyword = undefined;
+        this.filteredSubmissions = this.submissions;
       });
     } catch (e) {
       this.toastMessageService.displayError(e);
     }
+  }
+
+  applySearchFilter() {
+    if (!this.searchKeyword) {
+      this.filteredSubmissions = this.submissions;
+    }
+    this.filteredSubmissions = this.submissions
+      .filter(submission => submission.eclaimId?.includes(this.searchKeyword!) || submission.name?.includes(this.searchKeyword!));
   }
 
   async onMarkSubmissionAsPaid(submission: InsuranceSubmissionWithBills) {
